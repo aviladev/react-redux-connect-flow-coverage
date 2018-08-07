6a0b27102c32ae2e3c31ae6c43fdbb9c73ec1046
Pedro Ávila <pedroavila95@gmail.com>
2018-08-07 17:13:06 -0300

    Add redux

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 // @flow
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 
+import reducer from './reducer'
 import App from './App'
+
+const initial = ['Ice Cream', 'Chocolate', 'Pizza']
 
 // flow complains about `getElementById` possibly return `null`
 const root = document.getElementById('root')
-if (root) ReactDOM.render(<App />, root)
+if (root) ReactDOM.render(
+  <Provider store={createStore(reducer, initial)}>
+    <App />
+  </Provider>
+  , root
+)
---
+++ src/reducer.js
@@ -0,0 +1,6 @@
+// @flow
+export type State = $ReadOnlyArray<string>
+
+export default (state: State = [], action: {}) => {
+  return state
+}
